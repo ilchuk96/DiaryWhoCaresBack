1bf4b20736e021de885aabe4f9fca72b072d3df8
Ivan <ivanilchuk96@gmail.com>
2019-03-26 16:19:04 +0300

    plots without tags

--- html/get_files.py
+++ html/get_files.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import bs4
-
+import os.path
+import re
 
 wiki_url = 'http://ru.wikipedia.nom.al'
 with requests.get(wiki_url + '/wiki/Категория:Фильмы_по_алфавиту') as r:
@@ -9,15 +10,16 @@ with requests.get(wiki_url + '/wiki/Категория:Фильмы_по_алф�
 film_list = soup.find('div', {'class': 'mw-content-ltr'})
 items = film_list.find_all('li')
 for item in items:
-    try:
-        if item.find('div', {'class': 'CategoryTreeSection'}):
-            continue
-        movie_link = item.find('a').get('href')
-        movie_title = item.find('a').get('title')
-        film_url = wiki_url + movie_link
-        print(film_url)
-        with requests.get(film_url) as film_r:
-            film = BeautifulSoup(film_r.text)
+    if item.find('div', {'class': 'CategoryTreeSection'}):
+        continue
+    movie_link = item.find('a').get('href')
+    movie_title = item.find('a').get('title').replace('/', '\\')
+    if os.path.isfile('0-9/' + movie_title):
+        print("Уже записано.")
+        continue
+    film_url = wiki_url + movie_link
+    with requests.get(film_url) as film_r:
+        film = BeautifulSoup(film_r.text)
         cur = film.h2
         found = False
         ans = ''
@@ -26,14 +28,12 @@ for item in items:
                 found = True
                 cur = cur.next
                 continue
-            if found:
+            if found and cur.name == 'p':
                 ans += str(cur)
-            if cur.name == 'h2':
+            if found and cur.name == 'h2':
                 break
             cur = cur.next
-        print(ans)
-        break
-        with open('0-9/' + movie_title, 'w') as output_file:
-            output_file.write(text.text)
-    except Exception as e:
-        print(e)
+    ans = re.sub('<.*?>|[.*?]|Сюжет|В ролях', '', ans)
+    with open('0-9/' + movie_title, 'w') as output_file:
+        output_file.write(ans)
+    print('Записано.')
