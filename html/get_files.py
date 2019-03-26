8d617d19be7d989f86ea46e229aba267ef751e42
Ivan <ivanilchuk96@gmail.com>
2019-03-26 17:12:52 +0300

    added saving json and img

--- html/get_files.py
+++ html/get_files.py
@@ -1,10 +1,12 @@
+import shutil
 import requests
 from bs4 import BeautifulSoup
 import bs4
 import os.path
 import re
+import json
 
-starting_ind = 1
+starting_ind = 0
 pages = ['0-9', 'А', 'Б', 'В', 'Г', 'Д', 'Ж', 'У', 'З', 'И', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
          'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я']
 wiki_url = 'http://ru.wikipedia.nom.al'
@@ -25,26 +27,43 @@ for letter in pages[starting_ind:]:
             continue
         movie_link = item.find('a').get('href')
         movie_title = item.find('a').get('title').replace('/', '\\')
-        if os.path.isfile('0-9/' + movie_title):
+        if os.path.isdir('0-9/' + movie_title):
             print("Уже записано.")
             continue
+
         film_url = wiki_url + movie_link
         with requests.get(film_url) as film_r:
             film = BeautifulSoup(film_r.text)
-            cur = film.h2
-            found = False
-            ans = ''
-            while cur is not None:
-                if not isinstance(cur, bs4.NavigableString) and cur.find('span', {'id': 'Сюжет'}) is not None:
-                    found = True
-                    cur = cur.next
-                    continue
-                if found and cur.name == 'p':
-                    ans += str(cur)
-                if found and cur.name == 'h2':
-                    break
+
+        description = film.find('div', {'class': 'mw-parser-output'}).find('p')
+        description = re.sub('<.*?>|[.*?]', '', description.text)
+
+        cur = film.h2
+        found = False
+        plot = ''
+        while cur is not None:
+            if not isinstance(cur, bs4.NavigableString) and cur.find('span', {'id': 'Сюжет'}) is not None:
+                found = True
                 cur = cur.next
-        ans = re.sub('<.*?>|[.*?]|Сюжет|В ролях', '', ans)
-        with open(letter + '/' + movie_title, 'w') as output_file:
-            output_file.write(ans)
+                continue
+            if found and cur.name == 'p':
+                plot += str(cur)
+            if found and cur.name == 'h2':
+                break
+            cur = cur.next
+        plot = re.sub('<.*?>|[.*?]|Сюжет|В ролях', '', plot)
+        os.mkdir(letter + '/' + movie_title)
+
+        ans = {'id': movie_title, 'title': movie_title, 'description': description, 'plot': plot}
+        with open(letter + '/' + movie_title + '/' + movie_title + '.json', 'w') as output_file:
+            json.dump(ans, output_file, ensure_ascii=False)
+
+        img = film.find('img', {'alt': 'Постер фильма'})
+        if img is not None:
+            img_url = 'http:' + img.get('src')
+            extension = img_url.split('.')[-1]
+            response = requests.get(img_url, stream=True)
+            with open(letter + '/' + movie_title + '/' + movie_title + '.' + extension, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+
         print('Записано.')
