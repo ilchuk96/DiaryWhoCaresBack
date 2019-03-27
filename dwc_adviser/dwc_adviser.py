069966ae66ad89bd969597824207f714b623dbea
Vladislav Korablinov <vladislavneon1@mail.ru>
2019-03-27 12:24:49 +0300

    Add simple adviser interface

---
+++ dwc_adviser/dwc_adviser.py
@@ -0,0 +1,15 @@
+from movie_data import MovieData
+from adviser import RandomAdviser
+
+
+class MainAdviser:
+    def __init__(self):
+        self.MOVIE_DATA_CSV = 'movie_data.csv'
+
+        self.movie_data = MovieData()
+        self.movie_data.load_csv(self.MOVIE_DATA_CSV)
+
+        self.adviser = RandomAdviser(self.movie_data)
+
+    def make_suggestion(self, diary_entry):
+        return self.adviser.make_suggestion(diary_entry)
